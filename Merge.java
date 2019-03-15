27e39f9b23b17f617e959e692f76946e2f680db6
VictoriaSzy <vszymanowski00@stuy.edu>
2019-03-15 11:20:31 -0400

    Added frames for method and main

--- Merge.java
+++ Merge.java
@@ -1,3 +1,9 @@
 public class Merge {
-  
+  public static void main(String[] args) {
+    System.out.println() ;
+  }
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data) {
+
+  }
 }
